d33dc085946862c8f64d745311df9412d586a58c
petrikovandrey <aapetrikov@gmail.com>
2021-06-02 21:53:43 +0300

    fix

--- src/test.js
+++ src/test.js
@@ -1,5 +1,5 @@
 import healthMonitor from '../src/index.js';
-import {sortHealth} from "../src/index.js"; 
+
 
 test("health > 50", () => {
     const result = healthMonitor({ name: 'Маг', health: 55 });
@@ -15,19 +15,4 @@ test("health < 15", () => {
     const result = healthMonitor({ name: 'Маг', health: 10 });
     const expected = "critical";
     expect(result).toBe(expected);
-})
-
-test("sort descending", () => {
-    let list = [
-        { name: 'мечник', health: 10 },
-        { name: 'маг', health: 100 },
-        { name: 'лучник', health: 80 },
-    ];
-    const result = sortHealth(list);
-    const expected = [
-        { name: 'маг', health: 100 },
-        { name: 'лучник', health: 80 },
-        { name: 'мечник', health: 10 },
-    ];
-    expect(result).toEqual(expected);
 })
